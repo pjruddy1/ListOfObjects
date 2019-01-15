36873d8013869698f3b2629cb63016f9980a2858
Ruddyp <Ruddypj.pr@gmail.com>
2019-01-15 12:36:14 -0500

    Initial Commit

---
+++ ConsoleListOfObjects/PersonCar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleListOfObjects
+{
+    class PersonCar
+    {
+        public enum MakeName
+        {
+            NONE,
+            DODGE,
+            FORD,
+            CHEVY
+        }
+
+        private int _year;
+
+        private string _model;
+
+        private double _engine;
+
+        private MakeName _make;
+
+        private bool _isHardTop;
+
+        public bool IsHardTop
+        {
+            get { return _isHardTop; }
+            set { _isHardTop = value; }
+        }
+
+        public MakeName Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        public double Engine
+        {
+            get { return _engine; }
+            set { _engine = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+        public PersonCar()
+        {
+
+        }
+    }
+}
--- ConsoleListOfObjects/Program.cs
+++ ConsoleListOfObjects/Program.cs
@@ -10,6 +10,145 @@ namespace ConsoleListOfObjects
     {
         static void Main(string[] args)
         {
+
+            bool validResponse = false;
+            bool continueAdding = true;
+
+            string userResponse;
+            int year;
+            double engine;
+            PersonCar.MakeName carMake;
+
+            PersonCar newPersonCar = new PersonCar();
+
+            List<PersonCar> ListOfCars = new List<PersonCar>();
+
+            do
+            {
+                do
+                {
+                    Console.Write("Please enter car make. ");
+                    Enum.TryParse(Console.ReadLine().ToUpper(), out carMake);
+                    switch (carMake)
+                    {
+                        case PersonCar.MakeName.DODGE:
+                        case PersonCar.MakeName.FORD:
+                        case PersonCar.MakeName.CHEVY:
+                            validResponse = true;
+                            break;
+                        case PersonCar.MakeName.NONE:
+                            validResponse = false;
+                            break;
+                    }
+                } while (!validResponse);
+                newPersonCar.Make = carMake;
+
+                Console.Write("Please enter car model. ");
+                newPersonCar.Model = Console.ReadLine();
+
+                //
+                // Enter and validate car year
+                //
+
+                Console.WriteLine();
+                Console.Write("Please enter car year. ");
+                validResponse = int.TryParse(Console.ReadLine(), out year);
+                while (!validResponse)
+                {
+                    Console.Write("Please enter a correct year. ");
+                    validResponse = int.TryParse(Console.ReadLine(), out year);
+                }
+                newPersonCar.Year = year;
+
+                //
+                // Enter and validate engine size
+                //
+
+                Console.WriteLine();
+                Console.Write("Please enter engine size. ");
+                validResponse = double.TryParse(Console.ReadLine(), out engine);
+                while (!validResponse)
+                {
+                    Console.Write("Please enter a correct engine size. ");
+                    validResponse = double.TryParse(Console.ReadLine(), out engine);
+                }
+                newPersonCar.Engine = engine;
+
+                //
+                // Enter and Validate body top
+                //
+
+                Console.WriteLine();
+                do
+                {
+                    Console.Write("Is the body style a hard top? ");
+                    userResponse = Console.ReadLine().ToUpper();
+                    if (userResponse == "YES")
+                    {
+                        newPersonCar.IsHardTop = true;
+                        validResponse = true;
+                    }
+                    else if (userResponse == "NO")
+                    {
+                        newPersonCar.IsHardTop = false;
+                        validResponse = true;
+                    }
+                    else
+                    {
+                        validResponse = false;
+                    }
+
+                } while (!validResponse);
+
+                Console.WriteLine();
+                Console.Write("Press any key to add car to List. ");
+                Console.ReadKey();
+                ListOfCars.Add(newPersonCar);
+
+                //
+                // Enter and validate continue adding
+                //
+                do
+                {
+                    Console.Write("Would you like to add another car? ");
+                    userResponse = Console.ReadLine().ToUpper();
+                    if (userResponse == "YES")
+                    {
+                        continueAdding = true;
+                        validResponse = true;
+                    }
+                    else if (userResponse == "NO")
+                    {
+                        continueAdding = false;
+                        validResponse = true;
+                    }
+                    else
+                    {
+                        validResponse = false;
+                    }
+                } while (!validResponse);
+
+            } while (continueAdding);
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("Press any Key to Display the List of Cars. ");
+            Console.ReadKey();
+            foreach (PersonCar car in ListOfCars)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Make of Car: {car.Make}");
+                Console.WriteLine($"Model of Car: {car.Model}");
+                Console.WriteLine($"Year of Car: {car.Year}");
+
+                Console.WriteLine($"Size of Engine: {car.Engine}");
+                Console.WriteLine($"Is Body Style Hard Top: {car.IsHardTop}");
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("Press any Key to exit the application. ");
+            Console.ReadKey();
         }
     }
 }
